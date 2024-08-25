536e04f954bcf303e0bfe60603503b864562f727
pobegus <a.pobegus@gmail.com>
2024-08-25 16:56:08 +0200

    Добавлены новые тесты в tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import pytest
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -22,3 +23,120 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Charmed')
+        collector.set_book_genre('Charmed', 'Фантастика')
+        assert collector.get_book_genre('Charmed') == 'Фантастика'
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Charmed')
+        collector.set_book_genre('Charmed', 'Фантастика')
+        collector.add_new_book('The Shining')
+        collector.set_book_genre('The Shining', 'Ужасы')
+        books = collector.get_books_with_specific_genre('Фантастика')
+        assert books == ['Charmed']
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book('The Hobbit')
+        collector.set_book_genre('The Hobbit', 'Фантастика')
+        collector.add_new_book('The Shining')
+        collector.set_book_genre('The Shining', 'Ужасы')
+        collector.add_new_book('Notes on Sherlock Holmes')
+        collector.set_book_genre('Notes on Sherlock Holmes', 'Детективы')
+        collector.add_new_book('Frozen')
+        collector.set_book_genre('Frozen', 'Мультфильмы')
+        expected_books_for_children = ['The Hobbit', 'Frozen']
+        assert collector.get_books_for_children() == expected_books_for_children
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Charmed')
+        collector.set_book_genre('Charmed', 'Фантастика')
+        collector.add_book_in_favorites('Charmed')
+        assert 'Charmed' in collector.get_list_of_favorites_books()
+
+def test_get_list_of_favorites_books():
+    collector = BooksCollector()
+    collector.add_new_book('Charmed')
+    collector.set_book_genre('Charmed', 'Фантастика')
+    collector.add_book_in_favorites('Charmed')
+    favorites = collector.get_list_of_favorites_books()
+    assert favorites == ['Charmed']
+
+def test_get_list_of_favorites_books_successfully():
+    collector = BooksCollector()
+    collector.add_new_book("Charmed")
+    collector.set_book_genre("Charmed", "Фантастика")
+    collector.add_book_in_favorites("Charmed")
+    favorites = collector.get_list_of_favorites_books()
+    assert favorites == ["Charmed"]
+
+def test_delete_book_from_favorites_successfully():
+    collector = BooksCollector()
+    collector.add_new_book("Charmed")
+    collector.set_book_genre("Charmed", "Фантастика")
+    collector.add_book_in_favorites("Charmed")
+    collector.delete_book_from_favorites("Charmed")
+    assert "Charmed" not in collector.get_list_of_favorites_books()
+
+@pytest.mark.parametrize("books_and_genres, expected_books_for_children", [
+    (
+        [("The Hobbit", "Фантастика"), ("The Shining", "Ужасы"), ("Frozen", "Мультфильмы")],
+        ["The Hobbit", "Frozen"]
+    ),
+    (
+        [("The Shining", "Ужасы"), ("Notes on Sherlock Holmes", "Детективы")],
+        []
+    ),
+    (
+        [("The Hobbit", "Фантастика"), ("Frozen", "Мультфильмы")],
+        ["The Hobbit", "Frozen"]
+    )
+])
+def test_get_books_for_children(books_and_genres, expected_books_for_children):
+    collector = BooksCollector()
+    for book_name, genre in books_and_genres:
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre)
+
+    assert collector.get_books_for_children() == expected_books_for_children
+
+@pytest.mark.parametrize("books_and_genres, expected_books_genre", [
+    (
+        [("Charmed", "Фантастика"), ("The Shining", "Ужасы")],
+        {"Charmed": "Фантастика", "The Shining": "Ужасы"}
+    ),
+    (
+        [("Notes on Sherlock Holmes", "Детективы")],
+        {"Notes on Sherlock Holmes": "Детективы"}
+    ),
+    (
+        [],
+        {}
+    )
+])
+def test_get_books_genre(books_and_genres, expected_books_genre):
+    collector = BooksCollector()
+    for book_name, genre in books_and_genres:
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre)
+
+    assert collector.get_books_genre() == expected_books_genre
+
+@pytest.mark.parametrize("book_name, genre, expected_genre", [
+    ("Charmed", "Фантастика", "Фантастика"),
+    ("The Shining", "", ""),
+    ("None Book", None, None)
+])
+def test_get_book_genre(book_name, genre, expected_genre):
+    collector = BooksCollector()
+    if book_name != "None Book":
+        collector.add_new_book(book_name)
+        if genre is not None:
+            collector.set_book_genre(book_name, genre)
+
+    assert collector.get_book_genre(book_name) == expected_genre
